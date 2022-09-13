720d324ec8fb9a97c86cd180f457eb1c5dbb82fd
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-09-13 07:14:28 -0300

    feat: create initial config mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1a1a1a",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "#8763c2",
+    fontSize: 30,
+  },
+});
