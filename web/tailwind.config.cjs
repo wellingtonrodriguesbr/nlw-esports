c138ff5d507973eb1f52a4adf3b0b5a7372b8680
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-09-14 05:54:15 -0300

    feat: create homepage

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -4,12 +4,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: "Inter, sans-serif",
+        sans: ["Inter", "sans-serif"],
       },
       backgroundImage: {
         "nlw-gradient":
           "linear-gradient(89.86deg, #9572FC 13.08%, #43E7AD 40.94%, #E1D55D 90.57%)",
         galaxy: 'url("/background-galaxy.png")',
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
       },
     },
   },
