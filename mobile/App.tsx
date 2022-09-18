3a5499221b22f31cdb8ad0064e7d1ddd787c852e
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-09-18 20:52:54 -0300

    feat: push notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import { StatusBar } from "react-native";
 import { Background } from "./src/components/Background";
 import {
@@ -9,6 +10,11 @@ import {
 } from "@expo-google-fonts/inter";
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
+import { Subscription } from "expo-modules-core";
+import * as Notifications from "expo-notifications";
+
+import "./src/services/notificationConfigs";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,6 +23,34 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {});
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {});
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
 
   return (
     <Background>
